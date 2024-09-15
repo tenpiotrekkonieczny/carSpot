11a43161325691e9c0c7dc7da2f3d8fbb5585a96
Piotr Konieczny <piotrekprogramista@gmail.com>
2024-09-15 22:49:11 +0200

    add footer section

--- main.js
+++ main.js
@@ -27,3 +27,24 @@ new Swiper('.card-wrapper', {
   
     
   });
+
+  function toggleText() {
+    let text = document.getElementById("words");
+    if (text.style.display === "none") {
+      text.style.display = "block";
+    } else {
+      text.style.display = "none";
+    }
+  }
+
+
+  function toggleText1() {
+    let text1 = document.getElementById("words1");
+    if (text1.style.display === "none") {
+      text1.style.display = "block";
+    } else {
+      text1.style.display = "none";
+    }
+  }
+
+  
